af552028657243d598ea0d03bcb72aa5a80d2b76
sardor <sardorqobilov555@gmail.com>
2023-07-04 10:28:09 +0500

    project desktop version finished

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -29,39 +29,61 @@ function Header() {
 			</div>
 			<div className="navbar">
 				<div className="container">
-					<ul className='nav'>
-						<li className='nav-item'>
-							<a href="#!" className="nav-link active">Home</a>
-						</li>
-						<li className='nav-item'>
-							<a href="#!" className="nav-link">About</a>
-						</li>
-						<li className='nav-item'>
-							<a href="#!" className="nav-link">Listings</a>
-						</li>
-						<li className='nav-item'>
-							<a href="#!" className="nav-link">Services</a>
-						</li>
-						<li className='nav-item'>
-							<a href="#!" className="nav-link">Blogs</a>
-						</li>
-					</ul>
-					<div className='navbar-logo'>
-						<div className='house-icon'>
-							<img src={houseIcon} alt="house-icon" width='24px' height='24px' />
+					<div className="menu">
+						<div className="close">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+							</svg>
 						</div>
-						<p>Rezilla</p>
+						<ul className='nav'>
+							<li className='nav-item'>
+								<a href="#!" className="nav-link active">Home</a>
+							</li>
+							<li className='nav-item'>
+								<a href="#!" className="nav-link">About</a>
+							</li>
+							<li className='nav-item'>
+								<a href="#!" className="nav-link">Listings</a>
+							</li>
+							<li className='nav-item'>
+								<a href="#!" className="nav-link">Services</a>
+							</li>
+							<li className='nav-item'>
+								<a href="#!" className="nav-link">Blogs</a>
+							</li>
+						</ul>
+						<div className='navbar-logo'>
+							<div className='house-icon'>
+								<img src={houseIcon} alt="house-icon" width='24px' height='24px' />
+							</div>
+							<p>Rezilla</p>
+						</div>
+						<div className='register-box'>
+							<img src={userIcon} alt="user-icon" />
+							<div className='register-info'>
+								<a href="#!">Login/</a>
+								<a href="#!">Register</a>
+							</div>
+							<a href="#!" className='add-listing'>
+								<img src={houseIcon} alt="house-icon" />
+								Add Listing
+							</a>
+						</div>
 					</div>
-					<div className='register-box'>
-						<img src={userIcon} alt="user-icon" />
-						<div className='register-info'>
-							<a href="#!">Login/</a>
-							<a href="#!">Register</a>
+					<div className="media">
+						<div className='navbar-logo'>
+							<div className="flex-box">
+								<div className='house-icon'>
+									<img src={houseIcon} alt="house-icon" width='24px' height='24px' />
+								</div>
+								<p>Rezilla</p>
+							</div>
 						</div>
-						<a href="#!" className='add-listing'>
-							<img src={houseIcon} alt="house-icon" />
-							Add Listing
-						</a>
+						<div className="bars">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+							</svg>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/layouts/Testimonals.jsx
+++ src/layouts/Testimonals.jsx
@@ -6,75 +6,106 @@ import { Keyboard, Pagination, Navigation } from "swiper/modules";
 import lineImg from '../images/line.svg'
 import userImg from '../images/user-icon2.svg'
 import starIcon from '../images/stars-icon.svg'
+import agentImg from '../images/agent-img.png'
 
 function Testimonals() {
 	return (
 		<div className='testimonals'>
 			<div className="container">
-				<div className="title">
-					<h4 className='blue-title'>TESTIMONIALS</h4>
-					<h2 className='title-text'>Look What Our Customers Say!</h2>
-					<p>Fusce venenatis tellus a felis scelerisque, non pulvinar est pellentesque.</p>
+				<div className="testimonals-sliders">
+					<div className="title-slider">
+						<Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+							<SwiperSlide>
+								<div className="title">
+									<h4 className='blue-title'>TESTIMONIALS</h4>
+									<h2 className='title-text'>Look What Our Customers Say!</h2>
+									<p>Fusce venenatis tellus a felis scelerisque, non pulvinar est pellentesque.</p>
+								</div>
+							</SwiperSlide>
+							<SwiperSlide>
+								<div className="title">
+									<h4 className='blue-title'>TESTIMONIALS</h4>
+									<h2 className='title-text'>Look What Our Customers Say!</h2>
+									<p>Fusce venenatis tellus a felis scelerisque, non pulvinar est pellentesque.</p>
+								</div>
+							</SwiperSlide>
+							<SwiperSlide>
+								<div className="title">
+									<h4 className='blue-title'>TESTIMONIALS</h4>
+									<h2 className='title-text'>Look What Our Customers Say!</h2>
+									<p>Fusce venenatis tellus a felis scelerisque, non pulvinar est pellentesque.</p>
+								</div>
+							</SwiperSlide>
+						</Swiper>
+					</div>
+
+					<div className="slider-box">
+						<Swiper
+							slidesPerView="1"
+							mousewheel={true}
+							direction="vertical"
+							modules={[Pagination]}
+							pagination={{ clickable: true }}
+							scrollbar={{ draggable: true }}
+							className="mySwiper"
+						>
+							<SwiperSlide>
+								<div className="card-box">
+									<img src={lineImg} alt="" />
+									<p>I highly recommend Jodi J. Appleby. She was attentive to our needs and worked tirelessly to find us the perfect home. We couldn't be happier with our new place!</p>
+									<div className='line'></div>
+									<div className="slider-box__footer">
+										<div className="user-box">
+											<img src={userImg} alt="" />
+											<p>Barbara D. Smith</p>
+										</div>
+										<img src={starIcon} alt="" />
+									</div>
+								</div>
+							</SwiperSlide>
+							<SwiperSlide>
+								<div className="card-box">
+									<img src={lineImg} alt="" />
+									<p>I highly recommend Jodi J. Appleby. She was attentive to our needs and worked tirelessly to find us the perfect home. We couldn't be happier with our new place!</p>
+									<div className='line'></div>
+									<div className="slider-box__footer">
+										<div className="user-box">
+											<img src={userImg} alt="" />
+											<p>Barbara D. Smith</p>
+										</div>
+										<img src={starIcon} alt="" />
+									</div>
+								</div>
+							</SwiperSlide>
+							<SwiperSlide>
+								<div className="card-box">
+									<img src={lineImg} alt="" />
+									<p>I highly recommend Jodi J. Appleby. She was attentive to our needs and worked tirelessly to find us the perfect home. We couldn't be happier with our new place!</p>
+									<div className='line'></div>
+									<div className="slider-box__footer">
+										<div className="user-box">
+											<img src={userImg} alt="" />
+											<p>Barbara D. Smith</p>
+										</div>
+										<img src={starIcon} alt="" />
+									</div>
+								</div>
+							</SwiperSlide>
+						</Swiper>
+					</div>
 				</div>
 
-				<div className="slider-box">
-					<Swiper
-						slidesPerView={1}
-						spaceBetween={10}
-						mousewheel={true}
-						direction="vertical"
-						keyboard={{
-							enabled: true,
-						}}
-						pagination={{
-							clickable: true,
-						}}
-						modules={[Keyboard, Pagination, Navigation]}
-						className="mySwiper"
-					>
-						<SwiperSlide>
-							<div className="card-box">
-								<img src={lineImg} alt="" />
-								<p>I highly recommend Jodi J. Appleby. She was attentive to our needs and worked tirelessly to find us the perfect home. We couldn't be happier with our new place!</p>
-								<div className='line'></div>
-								<div className="slider-box__footer">
-									<div className="user-box">
-										<img src={userImg} alt="" />
-										<p>Barbara D. Smith</p>
-									</div>
-									<img src={starIcon} alt="" />
-								</div>
-							</div>
-						</SwiperSlide>
-						<SwiperSlide>
-							<div className="card-box">
-								<img src={lineImg} alt="" />
-								<p>I highly recommend Jodi J. Appleby. She was attentive to our needs and worked tirelessly to find us the perfect home. We couldn't be happier with our new place!</p>
-								<div className='line'></div>
-								<div className="slider-box__footer">
-									<div className="user-box">
-										<img src={userImg} alt="" />
-										<p>Barbara D. Smith</p>
-									</div>
-									<img src={starIcon} alt="" />
-								</div>
-							</div>
-						</SwiperSlide>
-						<SwiperSlide>
-							<div className="card-box">
-								<img src={lineImg} alt="" />
-								<p>I highly recommend Jodi J. Appleby. She was attentive to our needs and worked tirelessly to find us the perfect home. We couldn't be happier with our new place!</p>
-								<div className='line'></div>
-								<div className="slider-box__footer">
-									<div className="user-box">
-										<img src={userImg} alt="" />
-										<p>Barbara D. Smith</p>
-									</div>
-									<img src={starIcon} alt="" />
-								</div>
-							</div>
-						</SwiperSlide>
-					</Swiper>
+				<div className="agent-sec">
+					<div className="agent-img">
+						<img src={agentImg} alt="" />
+					</div>
+					<div className="agent-info__box">
+						<div className="agent-info">
+							<h3>Become a Agent.</h3>
+							<p>Fusce venenatis tellus a felis scelerisque. venenatis tellus a felis scelerisque. </p>
+						</div>
+						<a href="#!">Register Now</a>
+					</div>
 				</div>
 			</div>
 		</div>
